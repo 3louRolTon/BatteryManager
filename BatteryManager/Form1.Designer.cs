d7d463a3fb21c5fe8e674565beae7fe5e79a07d2
_3lou_RolTon_ <38839179+3louRolTon@users.noreply.github.com>
2019-12-08 16:11:11 +0300

    Add menu

--- BatteryManager/Form1.Designer.cs
+++ BatteryManager/Form1.Designer.cs
@@ -29,12 +29,119 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.Exit_button = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.GlavToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Exit_button
+            // 
+            this.Exit_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Exit_button.Location = new System.Drawing.Point(757, 12);
+            this.Exit_button.Name = "Exit_button";
+            this.Exit_button.Size = new System.Drawing.Size(31, 23);
+            this.Exit_button.TabIndex = 1;
+            this.Exit_button.Text = "X";
+            this.Exit_button.UseVisualStyleBackColor = true;
+            this.Exit_button.Click += new System.EventHandler(this.Exit_button_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.GlavToolStripMenuItem,
+            this.TableToolStripMenuItem,
+            this.SettingsToolStripMenuItem,
+            this.AboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // GlavToolStripMenuItem
+            // 
+            this.GlavToolStripMenuItem.Name = "GlavToolStripMenuItem";
+            this.GlavToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.GlavToolStripMenuItem.Text = "Главное";
+            this.GlavToolStripMenuItem.Click += new System.EventHandler(this.GlavToolStripMenuItem_Click);
+            // 
+            // TableToolStripMenuItem
+            // 
+            this.TableToolStripMenuItem.Name = "TableToolStripMenuItem";
+            this.TableToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.TableToolStripMenuItem.Text = "Таблица";
+            // 
+            // SettingsToolStripMenuItem
+            // 
+            this.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem";
+            this.SettingsToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.SettingsToolStripMenuItem.Text = "Настройки";
+            // 
+            // AboutToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(116, 24);
+            this.AboutToolStripMenuItem.Text = "О программе";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ControlBox = false;
+            this.Controls.Add(this.Exit_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Battery Manager";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Exit_button;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem GlavToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
     }
 }
 
--- BatteryManager/Form1.cs
+++ BatteryManager/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,52 @@ namespace BatteryManager
 {
     public partial class Form1 : Form
     {
+        PowerStatus power = SystemInformation.PowerStatus;
+
         public Form1()
         {
             InitializeComponent();
+            TrayMenuContext();
+
+            this.ShowInTaskbar = false;
+
+            SystemEvents.PowerModeChanged += new PowerModeChangedEventHandler(SystemEvents_PowerModeChanged);
+
+        }
+
+        void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            label1.Text = ((int)(power.BatteryLifePercent * 100)).ToString();
+        }
+
+        private void TrayMenuContext()
+        {
+            this.notifyIcon1.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            this.notifyIcon1.ContextMenuStrip.Items.Add("Show", null, this.MenuShow_Click);
+            this.notifyIcon1.ContextMenuStrip.Items.Add("Exit", null, this.MenuExit_Click);
+        }
+
+        void MenuExit_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            Application.Exit();
+        }
+
+        void MenuShow_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Exit_button_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void GlavToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+            this.Hide();
         }
     }
 }
